204668d10f29cfd39b90603933bb304356cbfd1a
mavdoshina <avdoshina.m.e@yandex.ru>
2022-08-14 00:16:38 +0300

    домашнее задание для второго урока

---
+++ lesson-2/script.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {int} a 
+ * @param {int} b 
+ * @returns int
+ */
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
+ function summ (a, b) {
+    return a + b;
+  }
